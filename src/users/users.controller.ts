0167722bd97b589ee869df8e01af65bdb617c981
migara10 <migaraten@gmail.com>
2024-04-24 02:17:58 +0300

    add user DTO's

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,11 +4,14 @@ import {
     Delete,
     Get,
     Param,
+    ParseIntPipe,
     Patch,
     Post,
     Query,
   } from '@nestjs/common';
   import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user-dto';
+import { UpdateUserDto } from './dto/update-user.dto';
   
   @Controller('users')
   export class UsersController {
@@ -20,23 +23,23 @@ import {
     }
   
     @Get(':id')
-    findOne(@Param('id') id: string) {
-      return this.userService.findOne(+id);
+    findOne(@Param('id', ParseIntPipe) id: number) {
+      return this.userService.findOne(id);
     }
   
     @Post() //POST /users
-    create(@Body() user: {email: string, name:string, role:'INTERN' | 'ENGINEER' | 'ADMIN'}) {
-      return this.userService.create(user);
+    create(@Body() createUserDto: CreateUserDto) {
+      return this.userService.create(createUserDto);
     }
   
     @Patch(':id')
-    update(@Param('id') id: string, @Body() userUpdate: {name: string, email: string, role:'INTERN' | 'ENGINEER' | 'ADMIN'}) {
-        return this.userService.update(+id, userUpdate)
+    update(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto) {
+        return this.userService.update(id, updateUserDto)
     }
   
     @Delete(':id')
-    delete(@Param('id') id: string) {
-      return this.userService.delete(+id);
+    delete(@Param('id', ParseIntPipe) id: number) {
+      return this.userService.delete(id);
     }
   }
   
