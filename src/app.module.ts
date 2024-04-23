94a2aa49e0112f9d34136551fbc30400f72108c7
migara10 <migaraten@gmail.com>
2024-04-24 01:56:45 +0300

    modify user servise component

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController, UsersController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,38 +1,42 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  Query,
-} from '@nestjs/common';
-
-@Controller('users')
-export class UsersController {
-  @Get() // Get all users /users or /users?role=ADMIN
-  findAll(@Query('role') role?: string) {
-    return role;
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Query,
+  } from '@nestjs/common';
+  import { UsersService } from './users.service';
+  
+  @Controller('users')
+  export class UsersController {
+    constructor(private readonly userService: UsersService) {}
+  
+    @Get() // Get all users /users or /users?role=ADMIN
+    findAll(@Query('role') role?:  'INTERN' | 'ENGINEER' | 'ADMIN') {
+      return this.userService.findAll(role);
+    }
+  
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+      return this.userService.findOne(+id);
+    }
+  
+    @Post() //POST /users
+    create(@Body() user: {email: string, name:string, role:'INTERN' | 'ENGINEER' | 'ADMIN'}) {
+      return this.userService.create(user);
+    }
+  
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() userUpdate: {name: string, email: string, role:'INTERN' | 'ENGINEER' | 'ADMIN'}) {
+        return this.userService.update(+id, userUpdate)
+    }
+  
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+      return this.userService.delete(+id);
+    }
   }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return { id };
-  }
-
-  @Post() //POST /users
-  create(@Body() user: {}) {
-    return user;
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
-  }
-
-  @Delete(':id')
-  delete(@Param('id') id: String) {
-    return { id };
-  }
-}
+  
