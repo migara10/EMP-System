0af9e282d02ef98d97e17c265bb53ef9e6f7ad6e
migara10 <migaraten@gmail.com>
2024-04-23 23:19:56 +0300

    modifi user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
+  @Get() // Get all users /users or /users?role=ADMIN
+  findAll(@Query('role') role?: string) {
+    return role;
+  }
 
-    @Get() // Get all users /users
-    findAll(){
-        return 'migara'
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
 
+  @Post() //POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
 
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: String) {
+    return { id };
+  }
 }
